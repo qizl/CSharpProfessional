d18a6126e4453760d9109451a21519ecd198b401
qizl <q@qizl.cn>
2018-10-29 17:12:53 +0800

    -

--- CSharpProfessional.Tests/CSharp6/Chapter5/DisposableSample.cs
+++ CSharpProfessional.Tests/CSharp6/Chapter5/DisposableSample.cs
@@ -1,6 +1,6 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
-using static System.Console;
+using static System.Diagnostics.Debug;
 
 namespace CSharpProfessional.Tests.CSharp10.Chapter5
 {
