46a5b47d5e7e2eb0f1261e98cf0c1b91bd8e5104
qizhenlu <q@qizl.cn>
2019-03-12 18:17:49 +0800

    -

--- DotnetWinform/FormMain.cs
+++ DotnetWinform/FormMain.cs
@@ -1,11 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace DotnetWinform
@@ -15,6 +8,8 @@ namespace DotnetWinform
         public FormMain()
         {
             InitializeComponent();
+
+            this.StartPosition = FormStartPosition.CenterScreen;
         }
 
         private void buttonExit_Click(object sender, EventArgs e)
