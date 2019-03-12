b76808c26c37984807ea55a5cf5d4721f285c8df
qizhenlu <q@qizl.cn>
2019-03-12 14:39:06 +0800

    random 测试

---
+++ CSharpProfessional.Tests/CSharp8/RandomTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CSharpProfessional.Tests.CSharp8
+{
+    [TestClass]
+    public class RandomTests
+    {
+        [TestMethod]
+        public void GetNumberWithSameSeed()
+        {
+            for (var i = 0; i < 10; i++)
+            {
+                Debug.WriteLine(new Random().Next(100));
+                //Thread.Sleep(1);
+            }
+
+            var rad = new Random();
+            for (var i = 0; i < 10; i++)
+                Debug.WriteLine(rad.Next(100));
+        }
+
+        int GetRandomSeed()
+        {
+            var bytes = new byte[4];
+            var rng = new System.Security.Cryptography.RNGCryptoServiceProvider();
+            rng.GetBytes(bytes);
+            return BitConverter.ToInt32(bytes, 0);
+        }
+
+        [TestMethod]
+        public void GetNumber1()
+        {
+            for (var i = 0; i < 10; i++)
+                Debug.WriteLine(new Random(this.GetRandomSeed()).Next(100));
+        }
+    }
+}
