c3362e99628c3b479f34e5b1394aef4ae1fa2927
qizhenlu <q@qizl.cn>
2019-09-26 18:12:06 +0800

    添加新语法

---
+++ CSharpProfessional.Core.Tests/CSharp8/DefaultImplementationsOfInterfaceMembers.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Diagnostics;
+
+namespace CSharpProfessional.Core.Tests.CSharp8
+{
+    interface ILogger
+    {
+        void Info(DateTime time, string message);
+        void Info(string message) => Info(DateTime.Now, message);
+    }
+
+    class DebugLogger : ILogger
+    {
+        public void Info(DateTime time, string message) => Debug.WriteLine($"{time.ToString()}  {nameof(message)}:{message}");
+    }
+
+    [TestClass]
+    public class DefaultImplementationsOfInterfaceMembers
+    {
+        [TestMethod]
+        public void Test()
+        {
+            ILogger logger = new DebugLogger();
+
+            logger.Info("测试日志");
+        }
+    }
+}
---
+++ CSharpProfessional.Core.Tests/CSharp8/IndexTests.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Diagnostics;
+
+namespace CSharpProfessional.Core.Tests.CSharp8
+{
+    [TestClass]
+    public class IndexTests
+    {
+        [TestMethod]
+        public void Test()
+        {
+            Index b = 3;
+            Index e = 5;
+            var a = new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            Debug.WriteLine($"{a[b]},{a[e]}");
+        }
+    }
+}
