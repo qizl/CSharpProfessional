a5b7342913fb2ccdb31d2f8d904cdd877ee564b0
qizl <qizl@glodon.com>
2021-04-25 10:32:46 +0800

    -

---
+++ CSharpProfessional/10Part1/Task.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace CSharpProfessional._10Part1
+{
+    public class TaskDemo
+    {
+        private void throwException()
+        {
+            var a = 1;
+            var b = 0;
+            var c = a / b;
+        }
+
+        public async void TaskCannotCatch()
+        {
+            await Task.Run(() =>
+            {
+                this.throwException();
+            });
+        }
+
+        public async Task TaskCanCatch()
+        {
+            await Task.Run(() =>
+            {
+                this.throwException();
+            });
+        }
+
+        public void RunTask()
+        {
+            Task.Run(async () =>
+            {
+                try
+                {
+                    this.TaskCannotCatch();
+
+                    await this.TaskCanCatch();
+                }
+                catch (Exception ex) { }
+            });
+        }
+    }
+}
