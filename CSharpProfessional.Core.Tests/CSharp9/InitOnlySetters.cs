6ec180e5769c22c274168ecfa0ed0ae1f1d8dfe2
qizl <qizl@glodon.com>
2021-11-20 15:37:24 +0800

    C# 9 练习

---
+++ CSharpProfessional.Core.Tests/CSharp9/InitOnlySetters.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace CSharpProfessional.Tests.CSharp9
+{
+    [TestClass]
+    public class InitOnlySetters
+    {
+        public class Person
+        {
+            public string? FirstName { get; set; }
+            public string? LastName { get; set; }
+        }
+
+        public void InitPersion()
+        {
+            var person = new Person { FirstName = "Mads", LastName = "Torgersen" };
+        }
+
+        public class Person1
+        {
+            public string? FirstName { get; init; }
+            public string? LastName { get; init; }
+        }
+
+        [TestMethod]
+        public void InitOnly()
+        {
+            var person = new Person1 { FirstName = "Mads", LastName = "Nielsen" }; // OK
+            //person.LastName = "Torgersen"; // ERROR!
+        }
+
+        public class Person2
+        {
+            private readonly string firstName = "<unknown>";
+            private readonly string lastName = "<unknown>";
+
+            public string FirstName
+            {
+                get => firstName;
+                init => firstName = (value ?? throw new ArgumentNullException(nameof(FirstName)));
+            }
+            public string LastName
+            {
+                get => lastName;
+                init => lastName = (value ?? throw new ArgumentNullException(nameof(LastName)));
+            }
+        }
+    }
+}
---
+++ CSharpProfessional.Core.Tests/CSharp9/Records.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpProfessional.Core.Tests.CSharp9
+{
+    [TestClass]
+    public class Records
+    {
+        public record Person
+        {
+            public string? FirstName { get; init; }
+            public string? LastName { get; init; }
+        }
+
+        [TestMethod]
+        public void WithExpressions()
+        {
+            var person = new Person { FirstName = "Mads", LastName = "Nielsen" };
+            var otherPerson = person with { LastName = "Torgersen" };
+            var otherPerson1 = otherPerson with { LastName = $"{otherPerson.LastName} House" };
+        }
+    }
+}
